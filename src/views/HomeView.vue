ab1ea48b725083acfb1729cc4cdbeede4f72203c
vernecic <vidernecic1@gmail.com>
2025-06-28 15:38:55 +0200

    pie chart in a seperate div

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,211 +1,225 @@
 <template>
-  <div class="font-open text-grayish p-6 shadow-lg bg-white rounded-md">
-    <div class="flex justify-between">
-      <h1 class="text-4xl font-semibold">BudgetTracker</h1>
-      <button
-        class="bg-grayish text-white font-semibold py-1 px-2 rounded-md cursor-pointer shadow-md hover:shadow-lg hover:bg-zinc-800"
-        @click="reset"
-      >
-        Reset
-      </button>
-    </div>
+  <div class="flex gap-8">
+    <div class="font-open text-grayish p-6 shadow-lg bg-white rounded-md">
+      <div class="flex justify-between">
+        <h1 class="text-4xl font-semibold">BudgetTracker</h1>
+        <button
+          class="bg-grayish text-white font-semibold py-1 px-2 rounded-md cursor-pointer shadow-md hover:shadow-lg hover:bg-zinc-800"
+          @click="reset"
+        >
+          Reset
+        </button>
+      </div>
 
-    <div v-if="isError">
-      <h3 class="text-red-500 text-lg">Error: {{ errorMessage }}</h3>
-    </div>
-    <div
-      class="flex flex-col space-y-4 xl:space-y-0 xl:grid xl:grid-cols-2 xl:gap-8"
-    >
-      <div class="mt-5 space-y-4">
-        <form action="" class="flex flex-col" @submit.prevent="addExpense()">
-          <label for="expense" class="text-xl font-semibold">Expense (€)</label>
-          <input
-            type="number"
-            v-model="expense"
-            placeholder="Enter your expenses"
-            class="bg-zinc-100 focus:outline-none p-1 text-lg rounded-md placeholder:text-base placeholder:text-zinc-500"
-          />
-        </form>
-        <div class="mt-2 relative">
-          <h2 class="text-xl font-semibold">Expense type</h2>
-          <div
-            class="border border-slate-300 flex justify-between text-base p-1 rounded-md cursor-pointer"
-            @click="showModal"
-          >
-            <span>{{ trenutnaKategorija }}</span>
-            <img
-              src="../images/arrowdown.svg"
-              alt=""
-              class="transition-transform duration-100"
-              :class="{ 'rotate-180': isModal }"
-            />
-          </div>
-          <div
-            class="absolute z-50 bg-white flex flex-col text-base border border-slate-300 p-1 rounded-md mt-2"
-            v-if="isModal"
-          >
-            <span
-              v-for="(kategorija, index) in kategorije"
-              :key="index"
-              class="cursor-pointer hover:bg-slate-100 rounded px-1"
-              @click="selectKategorija(kategorija)"
-            >
-              {{ kategorija }}
-            </span>
-          </div>
-        </div>
-        <div class="mt-2">
-          <textarea
-            name=""
-            placeholder="Enter a description(optional)"
-            id=""
-            cols="30"
-            rows="10"
-            class="bg-zinc-100 focus:outline-none p-1 text-lg rounded-md resize-none h-16 placeholder:text-base placeholder:text-zinc-500"
-            v-model="exDescription"
-          ></textarea>
-        </div>
-        <div class="flex xl:justify-center items-center my-5">
-          <button
-            class="bg-grayish text-base font-semibold text-white rounded-md p-2 cursor-pointer shadow-md hover:shadow-lg hover:bg-zinc-800"
-            @click="addExpense()"
-          >
-            Add expense
-          </button>
-        </div>
+      <div v-if="isError">
+        <h3 class="text-red-500 text-lg">Error: {{ errorMessage }}</h3>
       </div>
-      <div class="mt-5 flex flex-col justify-between">
-        <form action="" class="flex flex-col" @submit.prevent="addIncome()">
-          <label for="expense" class="text-xl font-semibold">Income (€)</label>
-          <input
-            type="number"
-            v-model="income"
-            placeholder="Enter your income"
-            class="bg-zinc-100 focus:outline-none p-1 text-lg rounded-md placeholder:text-base placeholder:text-zinc-500"
-          />
-        </form>
-        <div>
-          <textarea
-            name=""
-            placeholder="Enter a description(optional)"
-            id=""
-            cols="30"
-            rows="10"
-            class="mt-5 xl:mt-0 bg-zinc-100 focus:outline-none p-1 text-lg rounded-md resize-none h-16 placeholder:text-base placeholder:text-zinc-500"
-            v-model="inDescription"
-          ></textarea>
-          <div class="flex xl:justify-center items-center my-5">
-            <button
-              class="bg-white text-grayish border-grayish border font-semibold rounded-md p-2 cursor-pointer shadow-md hover:shadow-lg hover:bg-zinc-100"
-              @click="addIncome()"
-            >
-              Add income
-            </button>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="">
       <div
         class="flex flex-col space-y-4 xl:space-y-0 xl:grid xl:grid-cols-2 xl:gap-8"
       >
-        <!-- expenses -->
-        <div class="p-2 rounded-md bg-zinc-100">
-          <h2 class="text-2xl font-semibold">📉 Expenses</h2>
+        <div class="mt-5 space-y-4">
+          <form action="" class="flex flex-col" @submit.prevent="addExpense()">
+            <label for="expense" class="text-xl font-semibold"
+              >Expense (€)</label
+            >
+            <input
+              type="number"
+              v-model="expense"
+              placeholder="Enter your expenses"
+              class="bg-zinc-100 focus:outline-none p-1 text-lg rounded-md placeholder:text-base placeholder:text-zinc-500"
+            />
+          </form>
+          <div class="mt-2 relative">
+            <h2 class="text-xl font-semibold">Expense type</h2>
+            <div
+              class="border border-slate-300 flex justify-between text-base p-1 rounded-md cursor-pointer"
+              @click="showModal"
+            >
+              <span>{{ trenutnaKategorija }}</span>
+              <img
+                src="../images/arrowdown.svg"
+                alt=""
+                class="transition-transform duration-100"
+                :class="{ 'rotate-180': isModal }"
+              />
+            </div>
+            <div
+              class="absolute z-50 bg-white flex flex-col text-base border border-slate-300 p-1 rounded-md mt-2"
+              v-if="isModal"
+            >
+              <span
+                v-for="(kategorija, index) in kategorije"
+                :key="index"
+                class="cursor-pointer hover:bg-slate-100 rounded px-1"
+                @click="selectKategorija(kategorija)"
+              >
+                {{ kategorija }}
+              </span>
+            </div>
+          </div>
+          <div class="mt-2">
+            <textarea
+              name=""
+              placeholder="Enter a description(optional)"
+              id=""
+              cols="30"
+              rows="10"
+              class="bg-zinc-100 focus:outline-none p-1 text-lg rounded-md resize-none h-16 placeholder:text-base placeholder:text-zinc-500"
+              v-model="exDescription"
+            ></textarea>
+          </div>
+          <div class="flex xl:justify-center items-center my-5">
+            <button
+              class="bg-grayish text-base font-semibold text-white rounded-md p-2 cursor-pointer shadow-md hover:shadow-lg hover:bg-zinc-800"
+              @click="addExpense()"
+            >
+              Add expense
+            </button>
+          </div>
+        </div>
+        <div class="mt-5 flex flex-col justify-between">
+          <form action="" class="flex flex-col" @submit.prevent="addIncome()">
+            <label for="expense" class="text-xl font-semibold"
+              >Income (€)</label
+            >
+            <input
+              type="number"
+              v-model="income"
+              placeholder="Enter your income"
+              class="bg-zinc-100 focus:outline-none p-1 text-lg rounded-md placeholder:text-base placeholder:text-zinc-500"
+            />
+          </form>
+          <div>
+            <textarea
+              name=""
+              placeholder="Enter a description(optional)"
+              id=""
+              cols="30"
+              rows="10"
+              class="mt-5 xl:mt-0 bg-zinc-100 focus:outline-none p-1 text-lg rounded-md resize-none h-16 placeholder:text-base placeholder:text-zinc-500"
+              v-model="inDescription"
+            ></textarea>
+            <div class="flex xl:justify-center items-center my-5">
+              <button
+                class="bg-white text-grayish border-grayish border font-semibold rounded-md p-2 cursor-pointer shadow-md hover:shadow-lg hover:bg-zinc-100"
+                @click="addIncome()"
+              >
+                Add income
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="">
+        <div
+          class="flex flex-col space-y-4 xl:space-y-0 xl:grid xl:grid-cols-2 xl:gap-8"
+        >
+          <!-- expenses -->
+          <div class="p-2 rounded-md bg-zinc-100">
+            <h2 class="text-2xl font-semibold">📉 Expenses</h2>
 
-          <div
-            v-for="(expense, index) in expenses"
-            :key="index"
-            class="flex justify-between mt-2 items-center cursor-pointer"
-            @click="showExpenseDesc(index)"
-          >
-            <div class="flex flex-col flex-1">
-              <div class="flex justify-between">
-                <span class="text-lg"
-                  ><span class="text-red-500 font-semibold">-</span>
-                  {{ expense.amount }}€</span
-                >
-                <div>
-                  <div class="flex gap-2">
-                    <span class="font-medium">{{ expense.type }}</span>
+            <div
+              v-for="(expense, index) in expenses"
+              :key="index"
+              class="flex justify-between mt-2 items-center cursor-pointer"
+              @click="showExpenseDesc(index)"
+            >
+              <div class="flex flex-col flex-1">
+                <div class="flex justify-between">
+                  <span class="text-lg"
+                    ><span class="text-red-500 font-semibold">-</span>
+                    {{ expense.amount }}€</span
+                  >
+                  <div>
+                    <div class="flex gap-2">
+                      <span class="font-medium">{{ expense.type }}</span>
 
-                    <img
-                      src="../images/trash.svg"
-                      alt=""
-                      @click="removeExpense(index)"
-                      class="cursor-pointer"
-                    />
+                      <img
+                        src="../images/trash.svg"
+                        alt=""
+                        @click="removeExpense(index)"
+                        class="cursor-pointer"
+                      />
+                    </div>
                   </div>
                 </div>
-              </div>
 
-              <div v-if="expenseDescIndex === index">
-                <span class="text-zinc-500 italic">{{ expense.desc }}</span>
+                <div v-if="expenseDescIndex === index">
+                  <span class="text-zinc-500 italic">{{ expense.desc }}</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="p-2 rounded-md bg-zinc-100">
+            <h2 class="text-2xl font-semibold">📈 Incomes</h2>
+            <div
+              v-for="(income, index) in incomes"
+              :key="index"
+              class="flex mt-2 items-center cursor-pointer"
+              @click="showIncomeDesc(index)"
+            >
+              <div class="flex flex-col flex-1">
+                <div class="flex justify-between">
+                  <span class="text-lg"
+                    ><span class="text-green-500 font-semibold">+</span>
+                    {{ income.amount }}€</span
+                  >
+                  <img
+                    src="../images/trash.svg"
+                    alt=""
+                    class="cursor-pointer"
+                    @click="removeIncome(index)"
+                  />
+                </div>
+                <div v-if="incomeDescIndex === index">
+                  <span class="text-zinc-500 italic">{{ income.desc }}</span>
+                </div>
               </div>
             </div>
           </div>
         </div>
-        <div class="p-2 rounded-md bg-zinc-100">
-          <h2 class="text-2xl font-semibold">📈 Incomes</h2>
-          <div
-            v-for="(income, index) in incomes"
-            :key="index"
-            class="flex mt-2 items-center cursor-pointer"
-            @click="showIncomeDesc(index)"
+        <div class="grid grid-cols-2 gap-8">
+          <div class="mt-2 p-2 pt-2 bg-white rounded-md">
+            <h1>
+              <span class="font-semibold">Total expense: </span>
+              {{ fullexpenses }} €
+            </h1>
+          </div>
+          <div class="mt-2 p-2 pt-2 bg-white rounded-md">
+            <h1>
+              <span class="font-semibold">Total income:</span>
+              {{ fullincomes }} €
+            </h1>
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <h2 class="mt-5 text-xl">
+          💰 Current budget:
+          <span
+            :class="{
+              'text-red-500': currentBudget < 0,
+              'font-bold': currentBudget < 0,
+            }"
+            >{{ currentBudget }}€</span
           >
-            <div class="flex flex-col flex-1">
-              <div class="flex justify-between">
-                <span class="text-lg"
-                  ><span class="text-green-500 font-semibold">+</span>
-                  {{ income.amount }}€</span
-                >
-                <img
-                  src="../images/trash.svg"
-                  alt=""
-                  class="cursor-pointer"
-                  @click="removeIncome(index)"
-                />
-              </div>
-              <div v-if="incomeDescIndex === index">
-                <span class="text-zinc-500 italic">{{ income.desc }}</span>
-              </div>
-            </div>
+        </h2>
+      </div>
+    </div>
+    <div>
+      <div class="flex justify-center">
+        <div class="w-xs xl:w-xl">
+          <div class="bg-white p-4 rounded-md shadow-sm">
+            <h2 class="text-2xl font-semibold mb-4">📊 Expense Chart</h2>
+            <Pie
+              :data="chartData"
+              :options="chartOptions"
+              class="cursor-pointer"
+            />
           </div>
         </div>
       </div>
-      <div class="grid grid-cols-2 gap-8">
-        <div class="mt-2 p-2 pt-2 bg-white rounded-md">
-          <h1>
-            <span class="font-semibold">Total expense: </span>
-            {{ fullexpenses }} €
-          </h1>
-        </div>
-        <div class="mt-2 p-2 pt-2 bg-white rounded-md">
-          <h1>
-            <span class="font-semibold">Total income:</span>
-            {{ fullincomes }} €
-          </h1>
-        </div>
-      </div>
-    </div>
-    <div class="mt-8 w-xs xl:w-xl">
-      <h2 class="text-2xl font-semibold mb-4">📊 Expense Chart</h2>
-      <div class="bg-white p-4 rounded-md shadow-sm">
-        <Pie :data="chartData" :options="chartOptions" class="cursor-pointer" />
-      </div>
-    </div>
-
-    <div>
-      <h2 class="mt-5 text-xl">
-        💰 Current budget:
-        <span
-          :class="{
-            'text-red-500': currentBudget < 0,
-            'font-bold': currentBudget < 0,
-          }"
-          >{{ currentBudget }}€</span
-        >
-      </h2>
     </div>
   </div>
 </template>
@@ -236,13 +250,14 @@ const chartData = computed(() => {
       {
         data: data,
         backgroundColor: [
-          "#fcba03",
-          "#fc8003",
-          "#fc4a03",
           "#0fbd32",
+          "#fcba03",
           "#19d496",
+          "#fc8003",
           "#0dccde",
           "#1fb8f0",
+          "#fc4a03",
+
           "#1f84f0",
           "#0e47e3",
           "#7852e3",
